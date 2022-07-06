617f4ac9327db4605ffd870d52e75fc47c9cb69a
Jason Ruby <16628332+jason-ruby@users.noreply.github.com>
2022-07-06 12:21:44 -0500

    Initial commit

---
+++ keep-alive.ps1
@@ -0,0 +1,9 @@
+﻿# Future enhancements - use events rather than an infinite loop
+# $time = Read-Host -Prompt "How many minutes?"
+
+
+$wsh = New-Object -ComObject WScript.Shell
+while (1) {
+  $wsh.SendKeys('+{F15}')
+  Start-Sleep -seconds 59
+}
